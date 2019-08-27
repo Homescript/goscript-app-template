534b74cd4527e5716591f9ec5e4f476fe4c20a3e
Matthew Rothery <matthewrothery@outlook.com>
2019-08-28 09:17:26 +1000

    move api key and service id to the goscript apis

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import App from 'Containers';
 import goscript from 'custom-ui-api';
 
 // render a loading status while GoScript is configuring itself
 ReactDOM.render(<small>Loading...</small>, document.getElementById('root'));
 
-goscript.init(process.env.API_KEY, process.env.SERVICE_ID)
+goscript.init()
 .then(() => {
-    const history = createBrowserHistory();
-
     // setTimeout here to load the .less styles in dev
     setTimeout(() => {
         ReactDOM.render(
-            <BrowserRouter history={history} basename={`/app/${process.env.SERVICE_ID}`}>
+            <BrowserRouter basename={`/app/${process.env.SERVICE_ID}`}>
                 <App />
             </BrowserRouter>,
             document.getElementById('root')
